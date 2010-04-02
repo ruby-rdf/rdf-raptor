8f9114889fc2475fbdc8d6d936a0468f9726f442
Arto Bendiken <arto@bendiken.net>
2010-04-02 02:32:16 +0200

    Bumped version to 0.1.0.

--- lib/rdf/raptor/version.rb
+++ lib/rdf/raptor/version.rb
@@ -1,8 +1,8 @@
 module RDF; module Raptor
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 1
+    MINOR = 1
+    TINY  = 0
     EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY].join('.')
