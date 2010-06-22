60d8033111907ad4e24f68b45e34999a616bfe25
Arto Bendiken <arto@bendiken.net>
2010-06-22 02:55:16 +0200

    Changed the default file extension for the RDF::Raptor::Graphviz writer from '.gv' to the more widely-used '.dot'.

--- lib/rdf/raptor/graphviz.rb
+++ lib/rdf/raptor/graphviz.rb
@@ -15,17 +15,17 @@ module RDF::Raptor
   module Graphviz
     ##
     # Graphviz format specification.
-    # @see http://www.iana.org/assignments/media-types/text/vnd.graphviz
     #
     # @example Obtaining a Graphviz format class
-    #   RDF::Format.for(:gaphviz)       #=> RDF::Raptor::Graphviz::Format
-    #   RDF::Format.for("input.gv")
-    #   RDF::Format.for(:file_name      => "input.gv")
-    #   RDF::Format.for(:file_extension => "gv")
+    #   RDF::Format.for(:graphviz)     #=> RDF::Raptor::Graphviz::Format
+    #   RDF::Format.for("output.dot")
+    #   RDF::Format.for(:file_name      => "output.dot")
+    #   RDF::Format.for(:file_extension => "dot")
     #   RDF::Format.for(:content_type   => "text/vnd.graphviz")
     #
+    # @see http://www.iana.org/assignments/media-types/text/vnd.graphviz
     class Format < RDF::Raptor::Format
-      content_type     'text/vnd.graphviz', :aliases => ['application/x-graphviz', 'text/x-graphviz'], :extension => :gv
+      content_type     'text/vnd.graphviz', :aliases => ['application/x-graphviz', 'text/x-graphviz'], :extension => :dot # TODO: also .gv
       content_encoding 'utf-8'
       rapper_format    :dot
 
@@ -36,10 +36,10 @@ module RDF::Raptor
     # Graphviz serializer.
     #
     # @example Obtaining a Graphviz writer class
-    #   RDF::Writer.for(:graphviz)       #=> RDF::Raptor::Graphviz::Writer
-    #   RDF::Writer.for("output.gv")
-    #   RDF::Writer.for(:file_name      => "output.gv")
-    #   RDF::Writer.for(:file_extension => "gv")
+    #   RDF::Writer.for(:graphviz)     #=> RDF::Raptor::Graphviz::Writer
+    #   RDF::Writer.for("output.dot")
+    #   RDF::Writer.for(:file_name      => "output.dot")
+    #   RDF::Writer.for(:file_extension => "dot")
     #   RDF::Writer.for(:content_type   => "text/vnd.graphviz")
     #
     # @example Serializing RDF statements into a Graphviz file
