d5ea024631e78923cfd806a3e1ad2f0556d0ec10
Arto Bendiken <arto@bendiken.net>
2010-11-25 01:17:25 +0100

    Ensured that RDF::Raptor::FFI::Reader generates fresh random identifiers for Raptor's `_:genid[0-9]+` blank nodes, while preserving any user-specified blank node identifiers verbatim (closes #9).

--- lib/rdf/raptor/ffi.rb
+++ lib/rdf/raptor/ffi.rb
@@ -57,6 +57,7 @@ module RDF::Raptor
           block.call(RDF::Statement.new(*triple))
         end
       end
+      alias_method :each, :each_statement
 
       ##
       # @yield [triple]
@@ -64,7 +65,7 @@ module RDF::Raptor
       # @see   RDF::Reader#each_triple
       def each_triple(&block)
         statement_handler = Proc.new do |user_data, statement|
-          triple = V1_4::Statement.new(statement).to_triple
+          triple = V1_4::Statement.new(statement, self).to_triple
           block.call(triple)
         end
 
@@ -109,9 +110,24 @@ module RDF::Raptor
               V1_4.raptor_parse_chunk(parser, nil, 0, 1) # EOF
           end
         end
-
       end
-      alias_method :each, :each_statement
+
+      GENID = /^genid\d+$/
+
+      ##
+      # @param  [String] uri_str
+      # @return [RDF::URI]
+      def create_uri(uri_str)
+        RDF::URI.intern(uri_str)
+      end
+
+      ##
+      # @param  [String] node_id
+      # @return [RDF::Node]
+      def create_node(node_id)
+        @nodes ||= {}
+        @nodes[node_id] ||= RDF::Node.new(GENID === node_id ? nil : node_id)
+      end
     end # Reader
 
     ##
@@ -162,9 +178,9 @@ module RDF::Raptor
       # @see    RDF::Writer#write_triple
       def write_triple(subject, predicate, object)
         raptor_statement = V1_4::Statement.new
-        raptor_statement.subject = subject
+        raptor_statement.subject   = subject
         raptor_statement.predicate = predicate
-        raptor_statement.object = object
+        raptor_statement.object    = object
         begin
           unless V1_4.raptor_serialize_statement(@serializer, raptor_statement.to_ptr).zero?
             raise RDF::WriterError, "raptor_serialize_statement failed"
@@ -240,8 +256,13 @@ module RDF::Raptor
                :object_literal_datatype, :pointer,
                :object_literal_language, :pointer
 
-        def initialize(*args)
-          super
+        ##
+        # @param  [FFI::Pointer] pointer
+        # @param  [#create_node] factory
+        def initialize(pointer = nil, factory = nil)
+          super(pointer)
+          @factory = factory if factory
+
           # Objects we need to keep a Ruby reference to so they don't get
           # garbage collected out from under the C code we pass them to.
           @mp = {}
@@ -272,9 +293,9 @@ module RDF::Raptor
         def subject
           @subject ||= case self[:subject_type]
             when RAPTOR_IDENTIFIER_TYPE_RESOURCE
-              RDF::URI.intern(V1_4.raptor_uri_to_string(self[:subject]))
+              @factory.create_uri(V1_4.raptor_uri_to_string(self[:subject]))
             when RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
-              RDF::Node.new(self[:subject].read_string)
+              @factory.create_node(self[:subject].read_string)
           end
         end
 
@@ -286,14 +307,14 @@ module RDF::Raptor
         def subject=(resource)
           @subject = nil
           case resource
-          when RDF::Node
-            self[:subject] = @mp[:subject] = ::FFI::MemoryPointer.from_string(resource.id.to_s)
-            self[:subject_type] = RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
-          when RDF::URI
-            self[:subject] = @mp[:subject] = @raptor_uri_list.push(V1_4.raptor_new_uri(resource.to_s)).last
-            self[:subject_type] = RAPTOR_IDENTIFIER_TYPE_RESOURCE
-          else
-            raise ArgumentError, "subject must be of kind RDF::Node or RDF::URI"
+            when RDF::Node
+              self[:subject] = @mp[:subject] = ::FFI::MemoryPointer.from_string(resource.id.to_s)
+              self[:subject_type] = RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
+            when RDF::URI
+              self[:subject] = @mp[:subject] = @raptor_uri_list.push(V1_4.raptor_new_uri(resource.to_s)).last
+              self[:subject_type] = RAPTOR_IDENTIFIER_TYPE_RESOURCE
+            else
+              raise ArgumentError, "subject must be of kind RDF::Node or RDF::URI"
           end
           @subject = resource
         end
@@ -343,11 +364,11 @@ module RDF::Raptor
         def object
           @object ||= case self[:object_type]
             when RAPTOR_IDENTIFIER_TYPE_RESOURCE
-              RDF::URI.intern(V1_4.raptor_uri_to_string(self[:object]))
+              @factory.create_uri(V1_4.raptor_uri_to_string(self[:object]))
             when RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
-              RDF::Node.new(self[:object].read_string)
+              @factory.create_node(self[:object].read_string)
             when RAPTOR_IDENTIFIER_TYPE_LITERAL
-              str = self[:object].read_string.unpack("U*").pack("U*")
+              str = self[:object].read_string.unpack('U*').pack('U*')
               case
                 when self[:object_literal_language] && !self[:object_literal_language].null?
                   RDF::Literal.new(str, :language => self[:object_literal_language].read_string)
@@ -369,27 +390,27 @@ module RDF::Raptor
         def object=(value)
           @object = nil
           case value
-          when RDF::Node
-            self[:object] = @mp[:object] = ::FFI::MemoryPointer.from_string(value.id.to_s)
-            self[:object_type] = RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
-          when RDF::URI
-            self[:object] = @mp[:object] = @raptor_uri_list.push(V1_4.raptor_new_uri(value.to_s)).last
-            self[:object_type] = RAPTOR_IDENTIFIER_TYPE_RESOURCE
-          when RDF::Literal
-            self[:object_type] = RAPTOR_IDENTIFIER_TYPE_LITERAL
-            self[:object] = @mp[:object] = ::FFI::MemoryPointer.from_string(value.value)
-            self[:object_literal_datatype] = if value.datatype
-              @raptor_uri_list.push(V1_4.raptor_new_uri(value.datatype.to_s)).last
+            when RDF::Node
+              self[:object] = @mp[:object] = ::FFI::MemoryPointer.from_string(value.id.to_s)
+              self[:object_type] = RAPTOR_IDENTIFIER_TYPE_ANONYMOUS
+            when RDF::URI
+              self[:object] = @mp[:object] = @raptor_uri_list.push(V1_4.raptor_new_uri(value.to_s)).last
+              self[:object_type] = RAPTOR_IDENTIFIER_TYPE_RESOURCE
+            when RDF::Literal
+              self[:object_type] = RAPTOR_IDENTIFIER_TYPE_LITERAL
+              self[:object] = @mp[:object] = ::FFI::MemoryPointer.from_string(value.value)
+              self[:object_literal_datatype] = if value.datatype
+                @raptor_uri_list.push(V1_4.raptor_new_uri(value.datatype.to_s)).last
+              else
+                nil
+              end
+              self[:object_literal_language] = @mp[:object_literal_language] = if value.language?
+                ::FFI::MemoryPointer.from_string(value.language.to_s)
+              else
+                nil
+              end
             else
-              nil
-            end
-            self[:object_literal_language] = @mp[:object_literal_language] = if value.language?
-              ::FFI::MemoryPointer.from_string(value.language.to_s)
-            else
-              nil
-            end
-          else
-            raise ArgumentError, "object must be of type RDF::Node, RDF::URI or RDF::Literal"
+              raise ArgumentError, "object must be of type RDF::Node, RDF::URI or RDF::Literal"
           end
           @object = value
         end
