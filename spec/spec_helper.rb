cfc1e466c47a66f29a0cac6a2c8cb6439bf809cc
Arto Bendiken <arto@bendiken.net>
2010-12-27 16:58:16 +0100

    Updated the spec helper for RSpec 2.x compatibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'rdf/raptor'
 require 'rdf/spec'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.exclusion_filter = {:ruby => lambda { |version|
+    RUBY_VERSION.to_s !~ /^#{version}/
+  }}
 end
